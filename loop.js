2b4463706f42ebba22ae50b2f37ce4fa657cd80d
akintomide001 <akintomide001@gmail.com>
2022-10-19 08:31:13 +0100

    loop session

---
+++ loop.js
@@ -0,0 +1,15 @@
+//let i=1
+//while (i<=50) {
+    //if(i % 2==0){
+      //  console.log(i)
+    //}
+   // i++
+//}
+
+
+
+for( let i = 1;i<=50;i++){
+if( i % 2 !==0)
+continue;
+console.log(i)
+}
