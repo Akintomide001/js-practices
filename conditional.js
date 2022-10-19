62e86fb623fef371accfd636c9d48179652eb0c3
akintomide001 <akintomide001@gmail.com>
2022-10-19 09:11:02 +0100

    conditonal branching solution

---
+++ conditional.js
@@ -0,0 +1,82 @@
+const gender = "female";
+const age = 78
+
+if(gender =="male"){
+    if(age>=1 && age <= 5){
+        console.log("you can have a glass of milk");
+    }else {
+        console.log("we are sorry you can not have any other drink")
+    }
+} else if(gender =="female"){
+    if(age>=1 && age <= 5){
+        console.log("you can have a glass of milk");
+    }else {
+        console.log("we are sorry you can not have any other drink");
+    }
+}
+if(gender=="male"){
+    if(age>5 && age<11){
+        console.log("but you can have a chocolate drink")
+    }
+}
+if(gender=="female"){
+    if(age>5 && age<11){
+        console.log("but you can have a strawberry drink")
+    }
+}
+if(gender=="male"){
+    if(age>5 && age<16){
+        console.log("you can have ice cream or yogurt")
+    }
+}
+if(gender=="female"){
+    if(age>5 && age<16){
+        console.log("you can have ice cream or yogurt")
+    }
+}
+if(gender=="male"){
+    if(age>=10 && age<=18){
+        console.log("we have Fruit juice and Lime available")
+    }
+}
+if(gender=="female"){
+    if(age>=10 && age<=18){
+        console.log("we have (Fruit Juice and Strawberry ) or plain fruit juice avaiable")
+    }
+}
+if(gender=="male"){
+    if(age>=18 && age<=25){
+        console.log("you can go for vodka or whiskey")
+    }
+}
+if(gender=="female"){
+    if(age>=18 && age<=25){
+        console.log("you can go for tequila")
+    }
+}
+if(gender=="male"){
+    if(age>=26 && age<=40){
+        console.log("you can go for beer")
+    }
+}
+if(gender=="female"){
+    if(age>=26 && age<=60){
+        console.log("we have white and red wine for you")
+    }
+}
+if(gender=="male"){
+    if(age>=30 && age<=60){
+        console.log("we have white and red wine for you")
+    }
+}
+if(gender=="female"){
+    if(age>=61){
+        console.log("you can only take non alcholic drink and beverages")
+    }
+}
+if(gender=="male"){
+    if(age>=61){
+        console.log("you can only take non alcholic drink and beverages")
+    }
+}
+
